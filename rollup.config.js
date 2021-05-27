85dab38616a0a90d69a2e02db2b128974de4d783
Mohammad Ghiasi <emg110@gmail.com>
2021-05-27 15:11:59 +0430

    - fixed inverse option in web and module - added documentation (jsdocs) - fixed some typos - fixed some minor bugs - fixed note/xnote switch - added Algorand field validation (client side without jssdk) - fixed static html example styles and fields & js references - added output to examples/js/ folder to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -10,10 +10,10 @@ const babelConfig = {
 
 export default [{
   input: 'lib/browser.js',
-  output: { file: 'build/qrcode.js', format: 'iife', name: 'QRCode', exports: 'named' },
+  output: [{ file: 'build/qrcode.js', format: 'iife', name: 'QRCode', exports: 'named' },{ file: 'examples/js/qrcode.js', format: 'iife', name: 'QRCode', exports: 'named' }],
   plugins: [commonjs(), resolve(), babel(babelConfig), terser()]
 }, {
   input: 'helper/to-sjis-browser.js',
-  output: { file: 'build/qrcode.tosjis.js', format: 'iife', exports: 'none' },
+  output: [{ file: 'build/qrcode.tosjis.js', format: 'iife', exports: 'none' },{ file: 'examples/js/qrcode.tosjis.js', format: 'iife', exports: 'none' }],
   plugins: [commonjs(), resolve(), babel(babelConfig), terser()]
 }]
